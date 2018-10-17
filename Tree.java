49674c386021f7b081337ea414eb0e3a888e1c77
Eric Andrade <vloxflox@gmail.com>
2018-10-17 19:15:23 -0300

    tree com search e insert adicionado, testando

--- Tree.java
+++ Tree.java
@@ -2,9 +2,37 @@ public class Tree
 {
 	private Node root;
 
-	public insert(int key){
+	public Tree(){  
+        root = null;  
+    } 
+
+	public void insert(int key){
+		Node where = search(root, key);
+
+		if(where == null){
+			where = new Node(key);
+		}else{
+			System.out.print("we already got this");
+			return;
+		}
 
 	}
+
+	public Node search(Node current, int key) 
+	{ 
+	    //achou ou deu fail
+	    if (current==null || current.key==key){ //onde ele está ou deveria estar
+	    	return current;
+	    } 
+	    
+	    if (current.key > key){
+	        return search(current.left, key); 
+	    }else{
+	    	return search(current.right, key);
+	    }
+	  	     
+	} 
+
 
 
 }
