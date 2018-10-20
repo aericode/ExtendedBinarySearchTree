1f0d35f6c4421c28d6f38612f9ae211af5468f63
lucasgmpaiva <lucasgmpaiva1@gmail.com>
2018-10-20 12:38:33 -0300

    Realizando leitura de arquivos

--- Main.java
+++ Main.java
@@ -1,15 +1,36 @@
-public class Main{
-	public static void main(String[] args){
-		Tree tree = new Tree();
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Main {
+	public static void main(String[] args) {
+		/*Tree tree = new Tree();
 		tree.insertCall(4);
 		tree.insertCall(3);
 		tree.insertCall(3);
 		tree.insertCall(1);
 
-		if(tree.searchCall(5)){
-			System.out.println("achei");
-		}else{
-			System.out.println("não achei");
+		if(tree.searchCall(5)) {
+			System.out.println("Found!");
+		}else {
+			System.out.println("Not Found!");
+		}
+		 */
+		try {
+			FileReader file = new FileReader("ArquivodeComandos.");
+			BufferedReader readFile = new BufferedReader(file);
+			
+			String line = readFile.readLine();
+			while(line != null) {
+				System.out.println(line);
+				line = readFile.readLine();
+			}
+			
+			file.close();
+		} catch(IOException e) {
+			System.err.printf("Erro na abertura do arquivo: %s.\n",
+			e.getMessage());
 		}
 	}
 }
--- Tree.java
+++ Tree.java
@@ -1,10 +1,9 @@
-public class Tree
-{
+public class Tree {
+	
 	private Node root;
-
 	private int elementCount;
 
-	public Tree(){  
+	public Tree() {  
         root = null;
         elementCount = 0;
     }
@@ -12,10 +11,10 @@ public class Tree
 
 	//Insertion call
 	//Baseado em https://www.baeldung.com/java-binary-tree
-	public boolean insertCall(int key){
+	public boolean insertCall(int key) {
 		int auxCount = elementCount;
     	root = insert(root, key);
-    	if(auxCount!=elementCount){
+    	if(auxCount!=elementCount) {
     		return true;
     	}else{
     		return false;
@@ -23,33 +22,33 @@ public class Tree
 	}
 
 	private Node insert(Node current, int key){
-		if (current == null){
+		if (current == null) {
 			elementCount++; //aumenta em um o numero de elementos
 		    return new Node(key);
 		}
 
-		if (key < current.key){
+		if (key < current.key) {
 		    current.left = insert(current.left, key);
 		} else if (key > current.key){
 		    current.right = insert(current.right, key);
 		}
-		//retorna current em todas as iteraÃ§Ãµes, a menos que ache um local onde key estÃ¡
-		//se encontrar, retorna current tambÃ©m se encontrou o valor que deveria ser inserido
+		//retorna current em todas as iterações, a menos que ache um local onde key está
+		//se encontrar, retorna current também se encontrou o valor que deveria ser inserido
 		return current;
 	}
 
-	public boolean searchCall(int key){
+	public boolean searchCall(int key) {
 		return search(root, key);
 	}	
 
-	private boolean search(Node current, int key){
-		if(current == null){
+	private boolean search(Node current, int key) {
+		if(current == null) {
 			return false;
 		}else if (current.key == key){
 		    return true;
 		}
 
-		if (key < current.key){
+		if (key < current.key) {
 		   return search(current.left, key);
 		} else if (key > current.key){
 		   return search(current.right, key);
