0af65570bf0467e95a7c81c5bb4f665dd71fba82
Eric Andrade <vloxflox@gmail.com>
2018-10-23 12:57:56 -0300

    merge com operações 1 a 5

--- Main.java
+++ Main.java
@@ -1,39 +1,54 @@
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+
 public class Main {
 	public static void main(String[] args) {
-		/*Tree tree = new Tree();
-		tree.insert(4);
-		tree.insert(3);
-		tree.insert(3);
-		tree.insert(1);
-
-
-		if(tree.search(4)){
-			System.out.println("achei");
-		}else{
-			System.out.println("não achei");
-		}
-
-		tree.remove(4);
-
-
-		if(tree.search(4)){
-			System.out.println("achei");
-		}else{
-			System.out.println("não achei");
-		}
-
-		 */
+		Tree abb = new Tree();
 		try {
 			FileReader file = new FileReader("ArquivodeComandos");
 			BufferedReader readFile = new BufferedReader(file);
 			
 			String line = readFile.readLine();
 			while(line != null) {
-				System.out.println(line);
+				if(line.contains("INSIRA")) {
+					abb.insert(Integer.parseInt(line.substring(7)));
+				}
+				else if(line.contains("REMOVA")) {
+					abb.remove(Integer.parseInt(line.substring(7)));
+				}
+				else if(line.contains("ENENSIMO")) {
+					int enesimo = abb.enesimoElemento(Integer.parseInt(line.substring(9)));
+					System.out.println("O " + Integer.parseInt(line.substring(9)) + "º elemento é: " + enesimo);
+				}
+				else if(line.contains("POSICAO")) {
+					int posicao = abb.posicao(Integer.parseInt(line.substring(8)));
+					System.out.println("A Posição do elemento " + Integer.parseInt(line.substring(8)) + " é: " + posicao);
+				}
+				else if(line.contains("MEDIANA")) {
+					int mediana = abb.mediana();
+					System.out.println("A mediana da árvore é: " + mediana);
+				}
+				else if(line.contains("CHEIA")) {
+					if(abb.ehCheia()) {
+						System.out.println("A árvore é cheia!");
+					} else {
+						System.out.println("A árvore não é cheia!");
+					}
+				}
+				else if(line.contains("COMPLETA")) {
+					if(abb.ehCompleta()) {
+						System.out.println("A árvore é completa!");
+					} else {
+						System.out.println("A árvore não é completa!");
+					}
+				}
+				else if(line.contains("IMPRIMA")) {
+					System.out.println(abb.toString());
+				}
+				else {
+					System.out.println("Comando não reconhecido!");
+				}
 				line = readFile.readLine();
 			}
 			
